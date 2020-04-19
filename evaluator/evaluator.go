f6745770be7e16bd829e8b0771ff0a0b008c9630
makanai5610 <takumi.k.5610@gmail.com>
2020-04-19 15:35:17 +0900

    add if expression evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -14,7 +14,13 @@ var (
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
-		return evalStatement(node.Statements)
+		return evalStatements(node.Statements)
+
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
@@ -38,7 +44,7 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
-func evalStatement(stmts []ast.Statement) object.Object {
+func evalStatements(stmts []ast.Statement) object.Object {
 	var result object.Object
 
 	for _, statement := range stmts {
@@ -46,6 +52,30 @@ func evalStatement(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
